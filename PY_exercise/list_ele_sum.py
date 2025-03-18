b3ad925f861c025ccba1af3a2facc4923f999f60
Mayank Maiti <mayankmaiti03@gmail.com>
2025-03-18 09:31:29 +0530

    Create list_ele_sum.py

---
+++ PY_exercise/list_ele_sum.py
@@ -0,0 +1,12 @@
+a = [10, 20, 30, 40]
+
+# Initialize a variable to hold the sum
+res = 0
+
+# Loop through each value in the list
+for val in a:
+  
+    # Add the current value to the sum
+    res += val
+
+print(res)
