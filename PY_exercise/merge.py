4f4553db3525fbacde7e99ffe7c5bef21aa59658
Mayank Maiti <mayankmaiti03@gmail.com>
2025-04-15 20:03:52 +0530

    Create merge.py

---
+++ PY_exercise/merge.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+data1 = {
+  "name": ["Sally", "Mary", "John"],
+  "age": [50, 40, 30]
+}
+
+data2 = {
+  "name": ["Sally", "Peter", "Micky"],
+  "age": [77, 44, 22]
+}
+
+df1 = pd.DataFrame(data1)
+df2 = pd.DataFrame(data2)
+
+newdf = df1.merge(df2, how='right')
