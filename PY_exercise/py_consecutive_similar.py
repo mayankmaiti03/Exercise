687361818c44e79a444faf1eeccf5a4e28b76243
Mayank Maiti <mayankmaiti03@gmail.com>
2025-05-22 18:13:03 +0530

    Create py_consecutive_similar.py

---
+++ PY_exercise/py_consecutive_similar.py
@@ -0,0 +1,14 @@
+def consecutive_sim(l):
+  idx=0
+  res=[]
+
+  while idx<len(l):
+    start = idx
+    val = l[idx]
+    while idx<len(l) and l[idx]==val:
+      idx+=1
+    end=idx-1
+    res.append((val,start,end))
+  return res
+
+consecutive_sim([2, 3, 3, 3, 8, 8, 6, 7, 7])
